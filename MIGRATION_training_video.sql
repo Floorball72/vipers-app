cea439cada3fa6fb576d3bdc5626e941f96f92e4
Floorball72 <57669878+Floorball72@users.noreply.github.com>
2026-03-28 17:33:46 +0100

    Delete MIGRATION_training_video.sql

--- MIGRATION_training_video.sql
+++
@@ -1,4 +0,0 @@
--- Migration: Training & Video
-alter table videoanalysen add column if not exists kategorie text default 'gegner'
-  check (kategorie in ('gegner','highlights','taktik','allgemein'));
-alter table trainingseinheiten add column if not exists kategorie text default 'technik';
